f24e6d4ddac0d2719684986fce23f95b10d77bfe
masterzorag <masterzorag@gmail.com>
2016-11-30 18:18:03 +0100

    many charset variant
    - implement the set datatype, to support positional charsets
    - parse and store n lines as n independent charsets, one for every changing char
    - cleanup, poc

--- main.c
+++ main.c
@@ -3,12 +3,14 @@
 #include <string.h>
 #include "types.h"
 
+typedef struct {
+  u8 *data;
+  u8 len;
+} set;
 
 typedef struct {
-  u8 w_len;   // word lenght
-  u8 c_len;   // charset lenght
-  u8 *word;
-  u8 *cset;
+  set word;   // word
+  set *cset;  // many charset
   u8 *R;      // Repetitions, max use
 } ctx;
 
@@ -32,37 +34,35 @@ u8 scan(const u8 *item, const u8 *l, u8 mode, u8 *dst)
 {
   u8 *p = (u8*)item, ret = -1;
 
-  for(u8 i=0; i<*l; i++)
+  for(u8 i = 0; i < *l; i++)
   {
     switch(mode)
     {
-       case 0:    // output as CHAR
+       case CHAR:
           printf("%c", *p);
           break;
 
-       case 1:    // DUMP values
+       case DUMP:
           printf("%.2d/%.2d  %c  0x%.2x %.3d\n", i, *l, *p, *p, *p);
           break;
 
-       case 2:    // output as HEX
+       case HEX:
           printf("%.2x ", *p);
           break;
 
-       case 3: {  // STORE_IN dst[c_len]
+       case STORE_IN: {
           char t[2];
           strncpy(t, (char*)p, 1), t[1] = '\0';
           *(dst + i) = atoi(t);
           printf("%.2d/%.2d  %c  0x%.2x -> %.2d\n", i, *l, *p, atoi(t), *(dst + i));
           break; }
 
-       case 4: {  // and FIND dst
+       case FIND:
           //printf("%.2d/%.2d  %c  0x%.2x\n", i, *l, *p, *dst);
           if(*p == *dst) return i;
-          {
-            
-            
-          }
-          break; }
+
+          if(ret == -1) getchar();  // no found !!!
+          break;
 
        default :
           break;
@@ -80,23 +80,25 @@ void change(ctx *ctx, u8 *i)
 
   while(*i >= 0)
   {
-    p = ctx->word + *i;
+    p = ctx->word.data + *i;
 
-    if(*p == *(ctx->cset + ctx->c_len -1)) // last one
+    if(*p == *(ctx->cset->data + ctx->cset->len -1)) // last one
     {
       //printf("[%2x]", *p);
 
-      *p = *ctx->cset;     // first one
+      *p = *ctx->cset[*i].data;     // first one
       *i -= 1;
 
-      if(*i < 0) exit(-1); // trap
+      if(*i < 0) exit(-1); // trap exceptions
     }
     else
     {
-      u8 r = scan(ctx->cset, &ctx->c_len, FIND, p); // find in charset
+      set *d = &ctx->cset[*i];
+
+      u8 r = scan(d->data, &d->len, FIND, p); // find in charset
 //    if(r == ctx->c_len -1) getchar();
 
-      *p = *(ctx->cset + r +1);      // change to next one
+      *p = *(d->data + r +1);      // change to next one
 
       break;
     }
@@ -104,7 +106,7 @@ void change(ctx *ctx, u8 *i)
   p = NULL;
 }
 
-void parse_file(char *filename, ctx *ctx)
+u8 parse_file(char *filename, ctx *ctx)
 {
   FILE *fp;
   char *line = NULL;
@@ -114,58 +116,60 @@ void parse_file(char *filename, ctx *ctx)
   fp = fopen(filename, "r");
   if(!fp) exit(EXIT_FAILURE);
 
+  u8 max = ctx->word.len;
+  printf("reading max %d lines\n", max);
+
+  ctx->cset = malloc(sizeof(set) * max);
+  if(!ctx->cset) exit(EXIT_FAILURE);
+  printf("malloc for %zu @%p\n", sizeof(set*) * max, ctx->cset);
+
   u8 idx = 0;
-  while ((read = getline(&line, &len, fp)) != -1)
+  set *dst = NULL;
+
+  while ((read = getline(&line, &len, fp)) != -1
+  && (idx < max))
   {
     if(line[0] == '#') continue; // skip commented (#) lines
 
-    switch(idx)
-    {
-      // Setup charset, first line
-      case 0:
-        ctx->c_len = strlen(line) /* trim newline unconditionally */ -1;
-        ctx->cset  = malloc(ctx->c_len);
-        if(!ctx->cset) exit(-1);
+//    printf("Retrieved line of length %zu\n", read);
+//    printf("%s %zu\n", line, strlen(line));
 
-        strncpy((char*)ctx->cset, line, ctx->c_len); // store
-        ctx->cset[ctx->c_len] = '\0';
+    dst = &ctx->cset[idx];
 
-        scan(ctx->cset, &ctx->c_len, CHAR, NULL);    // report
-        idx++;
-        break;
+    dst->len = read -1;
+    dst->data = (u8*)strndup(line, dst->len);
 
-      // Setup Repetitions, second line
-      case 1:
-        ctx->R  = malloc(ctx->c_len);
-        if(!ctx->R) exit(-1);
+    printf("set %.2d/%.2d @%p:%d items\n", idx, max -1, dst, dst->len);
+    scan(dst->data, &dst->len, DUMP, NULL);          // report
 
-        scan((u8*)line, &ctx->c_len, STORE_IN, ctx->R); // store
-
-        scan(ctx->R, &ctx->c_len, DUMP, NULL);          // report
-        idx++;
-        break;
-
-      case 2:
-        break;
-
-      default :
-        break;
-
-    }
-
-    printf("Retrieved line of length %zu :\n", read);
-    printf("%s %zu", line, strlen(line));
-
+    idx++;
   }
-  free(line);
+
+  dst = NULL;
+  //if(line) free(line);
   fclose(fp);
+
+ //exit(1);
+  return idx;
 }
 
 
 
 int main(int argc, char **argv)
 {
-  ctx job;
+  ctx job;  // working context
+
+  if(argv[1])
+  {
+    job.word.len = strlen(argv[1]);
+    job.word.data  = malloc(job.word.len);
+    strncpy((char*)job.word.data, argv[1], job.word.len);
+
+
+    if(!job.word.data) exit(-1);
+
+  } else exit(EXIT_FAILURE);
+
 
   if(argv[2])
   {
@@ -176,46 +180,39 @@ int main(int argc, char **argv)
   }
 
 
-  // report
-  //pick(job.R, 0);
-
-  if(argv[1])
+  puts("report from main\n");
+  for(u8 i = 0; i < job.word.len; i++)
   {
-    job.w_len = strlen(argv[1]);
-    job.word  = malloc(job.w_len);
-    strncpy((char*)job.word, argv[1], job.w_len);
-
-    if(!job.word) exit(-1);
-
-  } else {
-    exit(EXIT_FAILURE);
+    scan(job.cset[i].data, &job.cset[i].len, DUMP, NULL);          // report
   }
+  exit(0);
 
-  printf("%s %u\n", job.word, job.w_len);
 
-  scan(job.word, &job.w_len, CHAR, NULL);
-  scan(job.word, &job.w_len, DUMP, NULL);
-  scan(job.word, &job.w_len, HEX, NULL);
+  printf("%s %u\n", job.word.data, job.word.len);
 
-  u8 n = job.w_len -1;
+  scan(job.word.data, &job.word.len, CHAR, NULL);
+  scan(job.word.data, &job.word.len, DUMP, NULL);
+  scan(job.word.data, &job.word.len, HEX, NULL);
+
+  u8 n = job.word.len -1;
   u32 c = 0;
   while(1)
   {
 
- //   scan(job.word, &job.w_len, CHAR, NULL);
-    printf("%s %d\r", job.word, job.w_len);
+ //   scan(job.word, &job.word.len, CHAR, NULL);
+    printf("%s %d\r", job.word.data, job.word.len);
 
-    if(memcmp(job.word, "acqua", job.w_len) == 0) break;
+    if(memcmp(job.word.data, "acqua", job.word.len) == 0) break;
 
     change(&job, &n);
 
-    n = job.w_len -1;
+    n = job.word.len -1;
     c++;
   }
 
   printf("%d\n", c);
 
-  free(job.word);
+  free(job.word.data);
   free(job.cset);
   free(job.R);
 
