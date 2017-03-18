81c80d1b3d4e3eaa595b8ac1189a81b41e7c53e2
masterzorag <masterzorag@gmail.com>
2017-03-18 22:16:08 +0100

    matrix_demo update

--- test/matrix.c
+++ test/matrix.c
@@ -1,15 +1,16 @@
 /*
-  gcc -o scan_matrix_demo scan_matrix.c -std=gnu99 -Wall
+  gcc -o matrix_demo matrix.c -std=gnu99 -Wall
 
   load a data matrix and check STDIN for (binary) items
 
-  $ ../bf -c test_7.HEX -x -b | ./scan_matrix_demo
+  $ ../bf -c test_7.HEX -x -b | ./matrix_demo
   Readed 25600b: 1280 items @0xeea250
   464101df2b5d342e046611c71ec11de8d173faf0 -> 20/20 @167
-  Found one at 168 !!!
-  Checked 75484 items
+  Found one at 167 !!!
+  Checked 112348 items
+
   or,
-  $ cut -c 1-20 /dev/urandom | ./scan_matrix_demo
+  $ cut -c 1-20 /dev/urandom | ./matrix_demo
   [30130.22/sec]
   [30074.36/sec]
   0324c53296690a1e3d81c893552fdcaea446ff90 -> 4/20 @829
@@ -25,10 +26,10 @@
 
 
 #include <time.h>
-clock_t startm, stopm;
+static clock_t startm, stopm;
 #define START if ((startm = clock()) == -1){ printf("Error calling clock"); exit(1); }
 #define STOP  if ((stopm  = clock()) == -1){ printf("Error calling clock"); exit(1); }
-#define PRINTTIME printf( "%6.3f seconds", ((double)stopm-startm) /CLOCKS_PER_SEC);
+#define PTIME printf( "%6.3f seconds", ((double)stopm-startm) /CLOCKS_PER_SEC);
 
 
 static int num_matrix_entries = 0;
@@ -67,7 +68,7 @@ static void *matrix_load()
   -1 if less than MIN_MATCH,
   else num of matching bytes
 */
-static signed char user_memcmp_v2(const unsigned char * const a, const unsigned char * const b)
+static signed char user_memcmp(const unsigned char * const a, const unsigned char * const b)
 {
   char count = 0;
   for(unsigned char i = 0; i < DATA_LEN; i++)
@@ -76,14 +77,12 @@ static signed char user_memcmp_v2(const unsigned char * const a, const unsigned 
     else count++;
   }
 
-  if(count != DATA_LEN)
-  {
-    if(count >= MIN_MATCH)
-      return count;
-    else
-      return -1;
-  }
-  else return 0; // whole match
+  if(count == DATA_LEN) return 0; // whole match
+
+  if(count >= MIN_MATCH)
+    return count;
+  else
+    return -1;
 }
 
 static char print_cmp(const unsigned char * const a, const unsigned char * const b)
@@ -103,17 +102,18 @@ static char print_cmp(const unsigned char * const a, const unsigned char * const
 
 /*
   Scan matrix targets for p, returns:
-  last best match honoring MIN_MATCH, else -1
+  best match honoring MIN_MATCH,
+  else -1
 */
 static signed int matrix_traverse(unsigned char * const M, unsigned char * const p)
 {
-  signed int last, res, best;
+  signed int best, last, res;
   best = last = res = -1;
   for(int i = 0; i < num_matrix_entries; i++) // ensure we scan whole table
   {
-    res = user_memcmp_v2(p, M + (i * DATA_LEN));
+    res = user_memcmp(p, M + (i * DATA_LEN));
 
-    if(res > best) last = i, best = res; // report the last best found
+    if(res > best) last = i, best = res; // report the best found
   }
   return last;
 }
@@ -127,8 +127,7 @@ static void matrix_read(unsigned char * const M, const int num)
 
   unsigned char * const p = M + (num * DATA_LEN);
 
-  for(int i = 0; i < DATA_LEN; i++)
-    printf("%.2x", *(p + i));
+  for(int i = 0; i < DATA_LEN; i++) printf("%.2x", *(p + i));
 
   puts("");
 }
@@ -140,8 +139,7 @@ static void matrix_dump(unsigned char * const M)
 {
   printf("data matrix @%p storing %d records\n", M, num_matrix_entries);
 
-  for(int i = 0; i < num_matrix_entries; i++) // scan matrix
-    matrix_read(M, i);
+  for(int i = 0; i < num_matrix_entries; i++) matrix_read(M, i);
 }
 
 
@@ -174,7 +172,7 @@ int main(int argc, char **argv)
 
         if(res == DATA_LEN) // exit if found one!
         {
-          printf("Found one at %d !!!\n", best +1); // (no row zero)
+          printf("Found one at %d !!!\n", best);
           break;
         }
       }
@@ -191,8 +189,6 @@ int main(int argc, char **argv)
     else break;
   }
   printf("Checked %d items\n", count); // done
-
-  matrix_read(M, best);
 
   free(M), M = NULL;
   free(p), p = NULL;
