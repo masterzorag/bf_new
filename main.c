8a2c0a37df71c59ee6777cd6a77346afe3312a67
masterzorag <masterzorag@gmail.com>
2017-04-03 22:45:48 +0200

    compact, remove switches

--- main.c
+++ main.c
@@ -65,44 +65,37 @@ int main(int argc, char **argv)
   job.wlen = 0;
   job.out_m = DRY_RUN; // default
 
-  job.work = parse_opt(argc, argv, &job);
-  DPRINTF("parse_opt()\tret:%d\n", job.work);
-  if(job.work)
+  for(u8 i = 0; i < 2; i++) // do two checks
   {
-    cleanup(&job); exit(EXIT_FAILURE);
-  }
-
-  job.work = parse_file(&job);
-  DPRINTF("parse_file()\tret:%d\n", job.work);
-  if(job.work)
-  {
-    fprintf(stderr, "[E] Please recheck and pass a valid config file with -c\n");
-    cleanup(&job); exit(EXIT_FAILURE);
+    switch(i)
+    {
+        case 0: job.work = parse_opt(argc, argv, &job); break;
+        case 1: job.work = parse_file(&job);            break;
+    }
+    if(job.work) { cleanup(&job); exit(EXIT_FAILURE); }
   }
 
   u8 *p = NULL;
-  if(1) // for verbose
+
+  #ifdef DEBUG
+  DPRINTF("Report from main, mode %u\n", job.mode);
+  for(u8 i = 0; i < job.wlen; i++)
   {
-    #ifdef DEBUG
-    DPRINTF("Report from main, mode %u\n", job.mode);
-    for(u8 i = 0; i < job.wlen; i++)
-    {
-      p = job.idx[i];
-      DPRINTF("idx %2d/%.2d @%p:\t%d items\n", i, job.wlen, p, p[0]);
-      scan(&p[1], &p[0], HEXDUMP, NULL);
-    }
-    DPRINTF("%zub %zub\n", sizeof(ctx), sizeof(void*));
-    DPRINTF("[I] Config passed, report data matrix:\n");
-    #endif
+    p = job.idx[i];
+    DPRINTF("idx %2d/%.2d @%p:\t%d items\n", i, job.wlen, p, p[0]);
+    scan(&p[1], &p[0], HEXDUMP, NULL);
   }
+  DPRINTF("%zub %zub\n", sizeof(ctx), sizeof(void*));
+  DPRINTF("[I] Config passed, report data matrix:\n");
+  #endif
 
   /* report data matrix */
   if(job.out_m == DRY_RUN)
   {
     if(job.numw) fprintf(stderr, "[I] Requested %u words!\n", job.numw);
 
-    dump_matrix(&job);
-    // in this case word is moved into the last one!
+    dump_matrix(&job); // in this case word is moved into the last one!
+
     cleanup(&job); exit(0);
   }
 
@@ -121,11 +114,10 @@ int main(int argc, char **argv)
     if(c %COUNT == 0) // output only every COUNT attempt
     #endif
     {
-      switch(job.out_m)
-      {
-        case BIN: bin2stdout(&job); break;                /* bin to STDOUT,   mode based */
-        default:  scan(p, &job.wlen, PRINT, NULL); break; /* standard output, mode based */
-      }
+      if(job.out_m == BIN)
+        bin2stdout(&job);                /* bin to STDOUT,   mode based */
+      else
+        scan(p, &job.wlen, PRINT, NULL); /* standard output, mode based */
 
       #ifdef COUNT // print timing info
       {
@@ -135,11 +127,9 @@ int main(int argc, char **argv)
       }
       #endif
 
-      switch(job.out_m)
-      {
-        case WORDLIST: printf("\n"); break; /* one-per-line output */
-        case QUIET:    printf("\r"); break; /* on-same-line output */
-      }
+      if(job.out_m == WORDLIST) printf("\n"); /* one-per-line output */
+      else
+      if(job.out_m == QUIET)    printf("\r"); /* on-same-line output */
     }
 
     if(job.numw && job.numw == c) break;
